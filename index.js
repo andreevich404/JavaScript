41bbf80c800442c1d794972f7f253000e6d46b9f
Ivan Andreevich <ivan_tolkachev.business@mail.ru>
2026-03-09 12:17:08 +0700

    refactor: replace lab 4 entry point with lab 5 runner

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const { runAskPassword } = require('./ask_password');
-const { runCallbackNotification } = require('./callbackNotification');
-const { runComputeSequential } = require('./computeSequential');
+const { runTask1 } = require('./task1');
+const { runWithPromises } = require('./task2');
+const { runComputeSequentialPromise } = require('./task3');
+const { runPeriodicSum } = require('./task4');
+const { runTask5 } = require('./task5');
+const { runTask6 } = require('./task6');
+const { runTask7 } = require('./task7');
 
-console.log('=== Этап 1: ask_password ===');
-runAskPassword(() => {
-	console.log('');
-	console.log('=== Этап 2: callback/notification ===');
-	runCallbackNotification(() => {
-		console.log('');
-		console.log('=== Этап 3: computeSequential ===');
-		runComputeSequential(() => {
-			console.log('');
-			console.log('=== Все этапы завершены ===');
-		});
+async function main() {
+	console.log('=== Задание 1: Promise разрешается только один раз ===');
+	await runTask1();
+
+	console.log('\n=== Задание 2: Промисификация функций ===');
+	await runWithPromises();
+
+	console.log('\n=== Задание 3: Последовательное вычисление F(x) через промисы ===');
+	await runComputeSequentialPromise();
+
+	console.log('\n=== Задание 4: Периодическое сложение (успешное выполнение) ===');
+	await runPeriodicSum(3, 5);
+	console.log('Периодическое суммирование завершено');
+
+	console.log('\n=== Задание 4: Периодическое сложение (вызов ошибки) ===');
+	await runPeriodicSum('abc', 5).catch(err => {
+		console.log('Ошибка:', err.message);
 	});
-});
+
+	console.log('\n=== Задание 5: Переписанные функции через async/await ===');
+	await runTask5();
+
+	console.log('\n=== Задание 6: Результат async-функции в обычной функции ===');
+	await runTask6();
+
+	console.log('\n=== Задание 7: Симуляция собеседования ===');
+	await runTask7();
+}
+
+main().catch(console.error);
